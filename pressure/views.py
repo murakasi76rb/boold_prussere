35805f98ec5a196e6c67f13325ff27e5b60c371e
Ruslan Bihun <bigun.ruslan@gmail.com>
2026-03-28 20:41:12 +0100

    add home method in views.py pressure

--- pressure/views.py
+++ pressure/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpRequest, HttpResponse
+
+def home(request:HttpRequest)->HttpResponse:
+    return render(request, 'base.html')
