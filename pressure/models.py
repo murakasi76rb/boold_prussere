864c7a98d20bce4e28e181517fade962f2f39568
Ruslan Bihun <bigun.ruslan@gmail.com>
2026-03-28 19:29:28 +0100

    add model User

--- pressure/models.py
+++ pressure/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import  AbstractUser
+
+class Gender(models.TextChoices):
+    MALE ='m', 'Male'
+    FEMALE = 'f', 'Female'
+
+
+
+class User(AbstractUser):
+    gender = models.CharField(max_length=1, choices=Gender, default=Gender.MALE)
+    age = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return  f'{self.first_name} {self.last_name} {self.age} {self.gender}'
