ceec2d7880d429a58582bd971146a00f65b798d2
Ruslan Bihun <bigun.ruslan@gmail.com>
2026-03-28 20:20:01 +0100

    add migrate 0002 0003, update user model

--- pressure/models.py
+++ pressure/models.py
@@ -11,7 +11,7 @@ class Gender(models.TextChoices):
 
 class User(AbstractUser):
     gender = models.CharField(max_length=1, choices=Gender, default=Gender.MALE)
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(blank=True, default=0)
 
     def __str__(self) -> str:
-        return  f'{self.first_name} {self.last_name} {self.age} {self.gender}'
+        return  f'{self.first_name} {self.last_name} {self.age} {self.get_gender_display()}' # type: ignore
