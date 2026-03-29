e404ff2646b21fbb8c6cb55db295aa1ed67210aa
Ruslan Bihun <bigun.ruslan@gmail.com>
2026-03-29 10:26:03 +0200

    add form FormBloodPressureRecord for BloodPressureRecord in pressure.forms.py

--- pressure/forms.py
+++ pressure/forms.py
@@ -1,2 +1,12 @@
 from django import forms
 from pressure.models import BloodPressureRecord
+
+class FormBloodPressureRecord(forms.ModelForm):
+    class Meat:
+        model =BloodPressureRecord
+        fielda = ['systolic', 'diastolic', 'pulse', 'patient']
+        lebales ={
+            'systolic': 'SYS',
+            'diastolic': 'DIA',
+            'pulse': 'HR',
+        }
